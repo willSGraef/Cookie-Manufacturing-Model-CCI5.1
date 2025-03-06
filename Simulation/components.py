4bd37169004a44d64399f0961cdbc437c657b91a
willSGraef <willgraef@gmail.com>
2025-03-06 12:07:30 -0600

    Added basic simulation saving functionality, rewrote signal logic for the simulation, created signal object in components.py.

--- Simulation/components.py
+++ Simulation/components.py
@@ -2,26 +2,26 @@
 #Author: William Graef, wgraef@uark.edu
 #RIOT LAB, UNIVERSITY OF ARKANSAS
 
-#Class description: Main class file for containers used in the model, silos, hoppers, etc.
+#Class description: Class for containers used in the model, silos, hoppers, etc.
 #There are sensors associated with each container which are also defined in this file
 
 class container():
 
-    #Consturctor initializes size of the container as well as the list of sensors attached
-    def __init__(self, size, sensors):
-        self.size = size
+    #Consturctor initializes weight of the container as well as the list of sensors attached
+    def __init__(self, weight, sensors):
+        self.weight = weight
         self.sensors = sensors
 
-    #--Size and sensor mutators
-    def set_size(self, new_size):
-        self.size = new_size
+    #--weight and sensor mutators
+    def set_weight(self, new_weight):
+        self.weight = new_weight
     
     #Add sensor to sensor list
     def add_sensor(self, new_sensor):
         self.sensors.append(new_sensor)
 
-    def get_size(self):
-        return self.size
+    def get_weight(self):
+        return self.weight
     
     def get_sensors(self):
         return self.sensors
@@ -44,21 +44,49 @@ class sensor():
     #--Value mutators
     def set_value(self, new_value):
         self.value = new_value
+
+    def up_value(self, increment):
+        self.value += increment
     
     def get_value(self):
         return self.value
     
-#Class description: Class file for the motor class, currently just has speed as a parameter for simplicity sake
+#Class description: Class for the motor: currently has voltage, rpm, cfm, and on_off parameters
 
 class motor:
 
     #Constructor initializes motor velocity at 0, implementation for further parameters will go here
     def __init__(self):
+        self.active = False
         self.velocity = 0
 
-    #Velocity mutators:
+    #Velocity mutator:
     def set_velocity(self, new_velocity):
         self.velocity = new_velocity
 
+    #Velocity getter:
     def get_velocity(self):
         return self.velocity
+    
+#Class description: Class for the different signals: currently has data_type, value, and address
+
+class signal:
+    #Constructor initializes data_type, value, and address based on paramters passed during construction
+    def __init__(self, value, address):
+        self.data_type = type(value)
+        self.value = value
+        self.address = address
+
+    #Value mutator:
+    def set_velocity(self, new_velocity):
+        self.velocity = new_velocity
+
+    #Getters:
+    def get_velocity(self):
+        return self.velocity
+    
+    def get_value(self):
+        return self.value
+    
+    def get_address(self):
+        return self.address
--- Simulation/main.py
+++ Simulation/main.py
@@ -8,13 +8,32 @@
 #Import dependencies
 import logging
 
+import json
+
 from pyModbusTCP.client import ModbusClient
 
-from components import container, motor, sensor
+from pyModbusTCP.utils import (decode_ieee, encode_ieee, long_list_to_word, word_list_to_long)
+
+from components import container, motor, sensor, signal
 #-----------------------------
 #Initialize modbus client and logging
 #-----------------------------
 
+#Modbus client with float reading and writing capabilities
+class FloatModbusClient(ModbusClient):
+    def read_float(self, address, number=1):
+        #Read float(s) with read holding registers.
+        reg_l = self.read_holding_registers(address, number * 2)
+        if reg_l:
+            return [decode_ieee(f) for f in word_list_to_long(reg_l)]
+        else:
+            return None
+
+    def write_float(self, address, floats_list):
+        #Write float(s) with write multiple registers.
+        b32_l = [encode_ieee(f) for f in floats_list]
+        b16_l = long_list_to_word(b32_l)
+        return self.write_multiple_registers(address, b16_l)
 
 #Set debug level for pyModbusTCP client to see frame changes, basically shows us all connection logs
 logging.basicConfig()
@@ -23,43 +42,77 @@ logging.getLogger('pyModbusTCP.client').setLevel(logging.DEBUG)
 #Init client using IP for the PLC acting as the 'hostname' and the port being 502 (Basic modbus communication port)
 #Enable auto open and auto close for our port so that it opens and closes for each invidiual request
 
-client = ModbusClient(host = "localhost", port = 502, auto_open= True, auto_close= True)
+client = FloatModbusClient(host = "localhost", port = 502, auto_open= True, auto_close= True)
 
-#-----------------------------
-#Begin function definitions
-#-----------------------------
+#Initialize all discrete signals to their default values [STATE, ADDRESS]:
 
-#TO DO: Add relevant motors and containers for each section of the model; resource handling, production, shipping handling
+signals = {
+    "start_recipe" : signal(False, 900), #BOOL ...
+    "rv_1" : signal(False, 700),
+    "rv_2" : signal(False, 701),
+    "rv_3" : signal(False, 702),
+    "dv" : signal(False, 703),
+    "vacuum" : signal(False, 704),
+    "mixer" : signal(False, 705), 
+    "hopper_alarm" : signal(False, 600), 
+    "flour_alarm" : signal(False, 601), 
+    "sugar_alarm" : signal(False, 602), 
+
+    #Initialize all analog signals to their default values
+    "vacuum_config" : signal(0, 1000), #INT ...
+    "mixer_config" : signal(0, 1001), 
+    "rv_1_config" : signal(0, 1002),
+    "rv_2_config" : signal(0, 1002),
+    "rv_3_config" : signal(0, 1002),
+
+    #Load cells for sugar, for REAL values make sure there
+    #is enough room for both 16 bit addresses
+    "lcs_1" : signal(0.000, 100), #REAL ...
+    "lcs_2" : signal(0.000, 102),
+    "lcs_3" : signal(0.000, 104),
+    "lcs_4" : signal(0.000, 106),
+
+    #Load cells for flour
+    "lcf_1" : signal(0.000, 108),
+    "lcf_2" : signal(0.000, 110),
+    "lcf_3" : signal(0.000, 112),
+    "lcf_4" : signal(0.000, 114),
+
+    #Load cell for hopper
+    "lch" : signal(0.000, 116)
+}
 
 #-----------------------------
 # RESOURCE HANDLING
 #-----------------------------
 
-#Container and LI definitions
-#LI: Level Indicator
+#Container and LI definitions, all container weights are in pounds
+#LC - Load Cell
 
-siloSize = 100
+silo_weight = 10000.0
 
 #Flour Silo
-flourSiloLIs = {
-    sensor(100),
-    sensor(75),
-    sensor(50)
+flourSiloLCs = {
+    "lcf_1" : sensor(250),
+    "lcf_2" : sensor(250),
+    "lcf_3" : sensor(250),
+    "lcf_4" : sensor(250)
 }
 
-flourSilo = container(100, flourSiloLIs)
+flourSilo = container(silo_weight, flourSiloLCs)
 
 #Sugar Silo
 
-sugarSiloLIs = {
-    sensor(100),
-    sensor(75),
-    sensor(50)
+sugarSiloLCs = {
+    "lcs_1" : sensor(250),
+    "lcs_2" : sensor(250),
+    "lcs_3" : sensor(250),
+    "lcs_4" : sensor(250)
 }
 
-sugarSilo = container(100, sugarSiloLIs)
+sugarSilo = container(silo_weight, sugarSiloLCs)
 
-hopper = container(20, {sensor(20)})
+hopper = container(400, {sensor(40)})
 
 #Motor and RV definitions
 #RV: Rotary Valve
@@ -68,8 +121,37 @@ flourRV = motor()
 sugarRV = motor()
 hopperRV = motor()
 
-diverterValve = motor()
+diverterValve = 0
 topValve = motor()
 
-vacuum = motor()
+fan = motor()
 mixer = motor()
+
+#BEGIN SIMULATION
+running = True
+while (running):
+    #Check to see if there is old sim data
+    with open("sim_data.json", 'r') as read_obj:
+        first_char = read_obj.read(1)
+    if not first_char:
+        #Data file is empty
+        print("Starting new simulation...")
+        with open("sim_data.json", "w") as file:
+            for s in signals.values:
+                json.dump(s.__dict__, file)
+    else:
+        #Data file is not empty
+        new_sim = input("Would you like to start a new simulation?\n")
+        if new_sim:
+            print("Starting new simulation...")
+            #Write current data to the data file and clear old data
+            with open("sim_data.json", "w") as file:
+                json.dump({}, file)
+                for s in signals.values:
+                    json.dump(s.__dict__, file)
+        else:
+            #Load existing data from the data file and clear old data
+            with open("sim_data.json", "r") as file:
+                json.dump({}, file)
+                for s in signals.values:
+                    signals = json.load(file)    
