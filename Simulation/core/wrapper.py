112603f5767d68fa0ee531b6eebdce78b165a2b1
willSGraef <willgraef@gmail.com>
2026-03-23 12:52:26 -0500

    Updated wrapper logic

--- Simulation/core/wrapper.py
+++ Simulation/core/wrapper.py
@@ -81,7 +81,7 @@ while not shutdown:
 
     # If wrapper conveying is on and running, increment the wrapper counter. 
     # If the wrapper counter has reached 1, reset it and set wrapper_conveying to false
-    if wrapper_conveying_value:
+    if wrapper_conveying_value and not wrapping_value:
         if counter >= 1:
             counter = 0
             wrapper_conveying_value = False
