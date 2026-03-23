8668f34edd0b770af7912a4dec6b2e42e0394099
willSGraef <willgraef@gmail.com>
2026-03-23 13:58:40 -0500

    Final box_maker fix

--- Simulation/core/box_maker.py
+++ Simulation/core/box_maker.py
@@ -77,11 +77,11 @@ while not shutdown:
         redis_client.set_value("box_maker", box_maker.get_value())
 
     # If photoelectric sensor 2 is off, set boxing to true
-    if not (ps_1 or ps_2 or conveying_1 or conveying_2):
+    if (not (ps_1 or ps_2 or conveying_1 or conveying_2)) and box_maker_value:
         boxing_value = True
 
     # If the box_maker is boxing increment the counter, if the counter has reached 3, reset the counter and set boxing to false
-    if boxing_value and box_maker_value:
+    if boxing_value:
         if counter >= 3 * COUNTER_TIME_SCALE:
             counter = 0
             boxing_value = False
